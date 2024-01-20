cb20ae0e05bbac2dfd63b09858e7051d5c6fbe55
wesley1248 <wesleytms.es@gmail.com>
2024-01-19 21:37:28 -0300

    Up

---
+++ Esfera/Esfera/Esfera/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Esfera
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool entradaValida = false;
+            double volumeValido = 0;
+            do
+            {
+                Console.Write("Digite o Raio do circulo: ");
+                string raio = Console.ReadLine();
+
+                if (double.TryParse(raio, out double raioValido))
+                {
+                    volumeValido = ((4.0 / 3.0) * 3.14159) * Math.Pow(raioValido, 3);
+                    entradaValida = true;
+                }
+                else
+                {
+                    Console.WriteLine("Entrada invalida.");
+                    Console.WriteLine();
+                }
+
+
+            }while (!entradaValida);
+
+            Console.WriteLine($"O volume é: {volumeValido:F3}");
+        }
+    }
+}
